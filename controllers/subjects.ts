fa81c0645525636a5761221ee1cb07e9446c19c6
Hikmahx <hikmayousuph@gmail.com>
2023-04-17 06:59:09 +0100

    createSubject updated for adding topics by id or object and checking if topic is added more than once

--- controllers/subjects.ts
+++ controllers/subjects.ts
@@ -1,6 +1,8 @@
 const Subject = require("../models/subject");
 import { Response, Request } from "express";
 import { validationResult } from "express-validator";
+import mongoose from "mongoose";
+const Topic = require("../models/topic");
 
 // @ route GET /api/subjects
 // @ desc  Fetch all subjects
@@ -42,9 +44,63 @@ export const createSubject = async (req: Request, res: Response) => {
   }
 
   try {
-    let subject = new Subject(req.body);
-    let newSubject = await subject.save();
-    res.status(201).json(newSubject);
+    // let subject = new Subject(req.body);
+    // let newSubject = await subject.save();
+    // res.status(201).json(newSubject);
+
+    const { title, topics } = req.body;
+
+    // Create new topics
+    // const newTopics: any[] = [];
+    // topics.forEach(async (topic: any) => {
+
+    const newTopics = await Promise.all(
+      topics.map(async (topic: any) => {
+        // Check if an id is given
+        if (mongoose.Types.ObjectId.isValid(topic)) {
+          // Check if the topic in already in DB
+          const existingTopic = await Topic.findById(topic);
+          if (existingTopic) {
+            console.log("this id already exist in the database");
+            // Use the existing topic's id
+            return existingTopic._id;
+          } else {
+            return res.status(400).json({ error: "Invalid topic ID" });
+          }
+          // If it is not an id but an object given
+        } else {
+          // Check if the topic already exists
+          const existingTopic = await Topic.findOne({
+            title: topic.title,
+            video: topic.video,
+            description: topic.description,
+          });
+          // If the topic has been created into this subject
+          if (existingTopic) {
+            console.log("this has already been added");
+            // Use the existing topic's id
+            return existingTopic._id;
+          } else {
+            // A new topic will be created
+            const { title, video, description } = topic;
+            const newTopic = new Topic({ title, video, description });
+            const savedTopic = await newTopic.save();
+            // newTopics.push(savedTopic._id);
+            return savedTopic._id;
+          }
+        }
+      })
+    );
+
+    // Create new subject
+    const subject = await new Subject({
+      title,
+      topics: newTopics,
+    });
+
+    const newSubject = await subject.save();
+    const populatedSubject = await Subject.findById(newSubject._id);
+    res.status(201).json(populatedSubject);
   } catch (err: any) {
     console.error(err.message);
     res.status(500).send("Server Error");
