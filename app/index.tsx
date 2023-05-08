1bd5926a69208a1a14e381231bb2799647eafdc6
Hikmahx <hikmayousuph@gmail.com>
2023-05-08 06:37:08 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Home = (): JSX.Element => {
+  return <View>
+    <Text>Home</Text>
+  </View>;
+};
+
+export default Home;
