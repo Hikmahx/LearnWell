86958269cea1a36d9ca04b63e65f1d617ac033b0
Hikmahx <hikmayousuph@gmail.com>
2024-04-18 19:55:07 +0100

    file modification with package update

--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -1,9 +1,10 @@
+process.env.EXPO_ROUTER_APP_ROOT = '../../app';
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      require.resolve("expo-router/babel"),
       [
         "module:react-native-dotenv",
         {
