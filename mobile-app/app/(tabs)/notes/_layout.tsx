f23749cecf6d2d53a0a3785e37cfb7d1dfe50c6d
Hikmahx <hikmayousuph@gmail.com>
2024-04-21 23:01:04 +0100

    notes layout updated

--- mobile-app/app/(tabs)/notes/_layout.tsx
+++ mobile-app/app/(tabs)/notes/_layout.tsx
@@ -1,11 +1,20 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
 
 const NotesLayout = () => {
   return (
-    <View>
-      <Text>NotesLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="[id]"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </>
   );
 };
 
