01ff22bfb1b5288cd12318f6df236e4bf1403a51
Hikmahx <hikmayousuph@gmail.com>
2024-04-22 17:30:27 +0100

    rich editor component

---
+++ mobile-app/components/MyNotes/new-note/PellRichEditor.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import {
+  Text,
+  Platform,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  ScrollView,
+  View,
+} from "react-native";
+import {
+  actions,
+  RichEditor,
+  RichToolbar,
+} from "react-native-pell-rich-editor";
+import tw from "../../../lib/tailwind";
+
+const handleHead = ({ tintColor }) => (
+  <Text style={{ color: tintColor }}>H1</Text>
+);
+
+const PellRichEditor = () => {
+  const richText = React.useRef();
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : null}
+        style={{ flex: 1 }}
+      >
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          <View style={{ flex: 1, backgroundColor: "transparent" }}>
+            <RichEditor
+              // initialFocus={true}
+              lastFocusEnd={true}
+              editorStyle={{ backgroundColor: "#faf2f3" }}
+              autoCorrect={true}
+              placeholder="Write your note here"
+              ref={richText}
+              // onChange={(descriptionText) => {
+              //   console.log("descriptionText:", descriptionText);
+              // }}
+              pasteAsPlainText={true}
+              initialHeight={250}
+            />
+          </View>
+        </ScrollView>
+
+        <RichToolbar
+          editor={richText}
+          actions={[
+            actions.setBold,
+            actions.setItalic,
+            actions.setUnderline,
+            actions.heading1,
+            actions.heading2,
+            actions.insertOrderedList,
+            actions.insertBulletsList,
+            actions.insertLink,
+            actions.undo,
+            actions.redo,
+            actions.table,
+            actions.setTitleFocusHandler,
+            actions.code,
+            // actions.setBackgroundColor,
+            actions.alignCenter,
+            actions.alignFull,
+            actions.alignLeft,
+            actions.alignRight,
+            // actions.setParagraph,
+            // actions.setTextColor,
+            actions.setStrikethrough,
+          ]}
+          iconMap={{ [actions.heading1]: handleHead }}
+          style={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            backgroundColor: "#494C5F",
+            borderRadius: 99,
+            marginBottom: 20,
+            padding: 2,
+            color: "white",
+          }}
+          iconTint={'white'}
+          // flatContainerStyle={{backgroundColor: '#black',
+          //  borderRadius: '999px'
+          // }}
+        />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+export default PellRichEditor;
