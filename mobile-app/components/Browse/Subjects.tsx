22e3d06b112eb5e11a8692d3de2d99105baa4546
Hikmahx <hikmayousuph@gmail.com>
2024-04-28 12:42:36 +0100

    Subject component updated

--- mobile-app/components/Browse/Subjects.tsx
+++ mobile-app/components/Browse/Subjects.tsx
@@ -19,7 +19,7 @@ const Subjects = () => {
           { backgroundColor: `${item.color}80` },
         ]}
       >
-        <View style={[tw`text-${item.color}`, styles.imageContainer]}>
+        <View style={[tw``, styles.imageContainer]}>
           <Svg width={20} height={20}>
             <Image
               href={item.svgCode}
