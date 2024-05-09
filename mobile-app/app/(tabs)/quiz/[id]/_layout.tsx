af4e16959d2ae80714cc4e8fbc65f831f7afa852
Hikmahx <hikmayousuph@gmail.com>
2024-05-09 18:16:22 +0100

    layout for quiz id

---
+++ mobile-app/app/(tabs)/quiz/[id]/_layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+const QuizIdLayout = () => {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="questions"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </>
+  );
+};
+
+export default QuizIdLayout;
