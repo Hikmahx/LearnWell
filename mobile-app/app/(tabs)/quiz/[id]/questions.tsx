dba922be31f3ef3c80a0d835cd23a9ce76b3b9df
Hikmahx <hikmayousuph@gmail.com>
2024-05-10 22:47:13 +0100

    questions page header completed

--- mobile-app/app/(tabs)/quiz/[id]/questions.tsx
+++ mobile-app/app/(tabs)/quiz/[id]/questions.tsx
@@ -1,10 +1,37 @@
-import { View, Text } from "react-native";
+import { View, Text, Animated } from "react-native";
 import React from "react";
+import { ArrowLongLeftIcon } from "react-native-heroicons/solid";
+import tw from "../../../../lib/tailwind";
+import ProgressBar from "@/components/Quiz/ProgressBar";
 
 const Questions = () => {
   return (
-    <View>
-      <Text>Questions</Text>
+    <View style={[tw`bg-white`, { minHeight: "100%" }]}>
+      <View>
+        <View style={tw`h-fit relative bg-blue`}>
+          <View style={tw`mt-10 mx-8`}>
+            <ArrowLongLeftIcon style={tw`mb-8 text-white `} />
+            <View style={tw`h-12 w-12 rounded-full bg-yellow mx-auto`}>
+              <Text style={tw`text-dark-gray text-xl font-bold m-auto`}>8</Text>
+            </View>
+            <Text style={tw`mt-8 font-bold text-xl text-white`}>
+              What is the result of 15 + 7 - 3?
+            </Text>
+
+            <View style={tw``}>
+              <Text style={tw`text-md font-thin pt-8 pb-4 text-white`}>
+                8/10
+              </Text>
+            </View>
+          </View>
+          <ProgressBar toValue={75} />
+        </View>
+        <View style={tw`bg-white h-auto`}></View>
+
+        <View
+          style={tw`px-7 max-w-lg w-full mx-auto mb-40 h-full mt-20`}
+        ></View>
+      </View>
     </View>
   );
 };
