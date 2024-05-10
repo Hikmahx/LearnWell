17536f72715b7b18a070a2086275fd1de76015f8
Hikmahx <hikmayousuph@gmail.com>
2024-05-10 22:59:51 +0100

    first commit

---
+++ mobile-app/components/Quiz/Options.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Options = () => {
+  return (
+    <View>
+      <Text>Options</Text>
+    </View>
+  );
+};
+
+export default Options;
