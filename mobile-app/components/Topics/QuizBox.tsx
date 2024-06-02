4d9d13f72d7968c5145d8170689508d4633436e4
Hikmahx <hikmayousuph@gmail.com>
2024-06-02 17:14:23 +0100

    quiz box updated

--- mobile-app/components/Topics/QuizBox.tsx
+++ mobile-app/components/Topics/QuizBox.tsx
@@ -7,26 +7,29 @@ import { Link } from "expo-router";
 
 const QuizBox = () => {
   return (
-    <Link href="/home" style={tw`w-full h-fix`}>
-      <View style={[tw`bg-blue rounded-lg p-5 relative mt-0.5 w-full`, styles.shadow]}>
-        <QuizImage style={tw`absolute -top-1/4 right-2`} />
+    <View
+      style={[tw`bg-blue rounded-lg p-5 relative mt-0.5 w-full`, styles.shadow]}
+    >
+      <View>
+        <QuizImage style={tw`absolute -top-1/3 right-2`} />
+        <Text style={tw`text-white font-bold mb-1 max-w-32`}>
+          Take a quiz to test your knowledge
+        </Text>
         <View style={tw`flex flex-row items-end justify-between w-full`}>
-          <View>
-            <Text style={tw`text-white font-bold mb-1 max-w-32`}>
-              Take a quiz to test your knowledge
-            </Text>
-            <Text style={tw`text-white mt-4 max-w-[280px]`}>
-              Taking a quiz not only tests your understanding but also
-              solidifies your learning, boosts confidence, and enhances
-              retention.
-            </Text>
-          </View>
+          <Text style={tw`text-white mt-4 max-w-[280px]`}>
+            Taking a quiz not only tests your understanding but also solidifies
+            your learning, boosts confidence, and enhances retention.
+          </Text>
+
           <Pressable style={tw`ml-8 `}>
-            <ArrowLongRightIcon style={tw`mb-2 text-yellow font-bold text-xl`} />
+            <ArrowLongRightIcon
+              style={tw`mb-2 text-yellow font-bold text-xl`}
+            />
+            <Link href="/quiz/11" style={tw`absolute inset-0 z-10`}></Link>
           </Pressable>
         </View>
       </View>
-    </Link>
+    </View>
   );
 };
 
