6b3cace51e9a0d11e60cbe352898074b4a59e764
Hikmahx <hikmayousuph@gmail.com>
2024-06-05 19:43:28 +0100

    tailwind set up

---
+++ admin/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        "blue": "#146BFB",
+        "soft-red": "#EA5353",
+        "yellow": "#FECF63",
+        "purple": "#9B51E0",
+        "green": "#42DA80",
+        "white": "#fff",
+        "gray": "#b5b5b5",
+        "dark-gray": "#494C5F",
+      },
+    },
+  },
+  plugins: [],
+}
+
