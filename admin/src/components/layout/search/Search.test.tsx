2da7d66d58f6f72179c1538346771d2433f0d65b
Hikmahx <hikmayousuph@gmail.com>
2024-06-24 11:36:39 +0100

    Search.test.tsx

--- admin/src/components/layout/search/Search.test.tsx
+++ admin/src/components/layout/search/Search.test.tsx
@@ -1,1 +1,62 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import configureStore, { MockStoreEnhanced } from "redux-mock-store";
+import "@testing-library/jest-dom/extend-expect";
+import { BrowserRouter } from "react-router-dom";
+import Search from "./Search";
+import { RootState } from "../../../redux/store";
+import userEvent from "@testing-library/user-event";
 
+const mockStore = configureStore<Partial<RootState>>([]);
+
+describe("Testing Search Component", () => {
+  let store: MockStoreEnhanced<unknown, {}>;
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    store = mockStore({
+      shared: {
+        showAside: false || true,
+        searchText: "",
+      },
+    });
+  });
+
+  test("renders the Search component", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Search />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    expect(screen.getByLabelText("Search")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Search")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /search/i })).toBeInTheDocument();
+  });
+
+  test("render form submission", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Search />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    const searchElement = screen.getByPlaceholderText("Search");
+    const formElement = screen.getByTestId("search-form");
+
+    fireEvent.change(searchElement, { target: { value: "test" } });
+    fireEvent.submit(formElement);
+
+    const mockDispatch = {
+      type: "shared/searchInput",
+      payload: "test",
+    };
+
+    store.dispatch(mockDispatch);
+  });
+});
